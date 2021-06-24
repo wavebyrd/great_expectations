4f1fdfac8a80c7bd01ab301f306280382435575a
William Shin <will@superconductive.com>
2021-06-24 15:22:36 -0700

    [DOCS] How to connect to : Sqlite database (#2951)
    * push with guide and test_script_runner updates

--- tests/integration/docusaurus/connecting_to_your_data/database/sqlite_python_example.py
+++ tests/integration/docusaurus/connecting_to_your_data/database/sqlite_python_example.py
@@ -3,53 +3,47 @@ from ruamel import yaml
 import great_expectations as ge
 from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
 
+CONNECTION_STRING = "sqlite:///data/yellow_tripdata.db"
+
 context = ge.get_context()
 
 datasource_config = {
-    "name": "taxi_datasource",
+    "name": "my_sqlite_datasource",
     "class_name": "Datasource",
-    "module_name": "great_expectations.datasource",
     "execution_engine": {
-        "module_name": "great_expectations.execution_engine",
-        "class_name": "PandasExecutionEngine",
+        "class_name": "SqlAlchemyExecutionEngine",
+        "connection_string": "sqlite://<PATH_TO_DB_FILE>",
     },
     "data_connectors": {
         "default_runtime_data_connector_name": {
             "class_name": "RuntimeDataConnector",
-            "module_name": "great_expectations.datasource.data_connector",
             "batch_identifiers": ["default_identifier_name"],
         },
         "default_inferred_data_connector_name": {
-            "class_name": "InferredAssetFilesystemDataConnector",
-            "base_directory": "<PATH_TO_YOUR_DATA_HERE>",
-            "default_regex": {"group_names": ["data_asset_name"], "pattern": "(.*)"},
+            "class_name": "InferredAssetSqlDataConnector",
+            "name": "whole_table",
         },
     },
 }
 
 # Please note this override is only to provide good UX for docs and tests.
 # In normal usage you'd set your path directly in the yaml above.
-datasource_config["data_connectors"]["default_inferred_data_connector_name"][
-    "base_directory"
-] = "../data/"
+datasource_config["execution_engine"]["connection_string"] = CONNECTION_STRING
 
 context.test_yaml_config(yaml.dump(datasource_config))
 
 context.add_datasource(**datasource_config)
 
-# Here is a RuntimeBatchRequest using a path to a single CSV file
+# Here is a RuntimeBatchRequest using a query
 batch_request = RuntimeBatchRequest(
-    datasource_name="taxi_datasource",
+    datasource_name="my_sqlite_datasource",
     data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="<YOUR_MEANINGFUL_NAME>",  # This can be anything that identifies this data_asset for you
-    runtime_parameters={"path": "<PATH_TO_YOUR_DATA_HERE>"},  # Add your path here.
+    data_asset_name="default_name",  # this can be anything that identifies this data
+    runtime_parameters={
+        "query": "SELECT * from yellow_tripdata_sample_2019_01 LIMIT 10"
+    },
     batch_identifiers={"default_identifier_name": "something_something"},
 )
-
-# Please note this override is only to provide good UX for docs and tests.
-# In normal usage you'd set your path directly in the BatchRequest above.
-batch_request.runtime_parameters["path"] = "./data/yellow_trip_data_sample_2019-01.csv"
-
 context.create_expectation_suite(
     expectation_suite_name="test_suite", overwrite_existing=True
 )
@@ -61,17 +55,12 @@ print(validator.head())
 # NOTE: The following code is only for testing and can be ignored by users.
 assert isinstance(validator, ge.validator.validator.Validator)
 
-# Here is a BatchRequest naming a data_asset
+# Here is a BatchRequest naming a table
 batch_request = BatchRequest(
-    datasource_name="taxi_datasource",
+    datasource_name="my_sqlite_datasource",
     data_connector_name="default_inferred_data_connector_name",
-    data_asset_name="<YOUR_DATA_ASSET_NAME>",
+    data_asset_name="yellow_tripdata_sample_2019_01",  # this is the name of the table you want to retrieve
 )
-
-# Please note this override is only to provide good UX for docs and tests.
-# In normal usage you'd set your data asset name directly in the BatchRequest above.
-batch_request.data_asset_name = "yellow_trip_data_sample_2019-01.csv"
-
 context.create_expectation_suite(
     expectation_suite_name="test_suite", overwrite_existing=True
 )
@@ -82,9 +71,9 @@ print(validator.head())
 
 # NOTE: The following code is only for testing and can be ignored by users.
 assert isinstance(validator, ge.validator.validator.Validator)
-assert [ds["name"] for ds in context.list_datasources()] == ["taxi_datasource"]
-assert "yellow_trip_data_sample_2018-01.csv" in set(
-    context.get_available_data_asset_names()["taxi_datasource"][
+assert [ds["name"] for ds in context.list_datasources()] == ["my_sqlite_datasource"]
+assert "yellow_tripdata_sample_2019_01" in set(
+    context.get_available_data_asset_names()["my_sqlite_datasource"][
         "default_inferred_data_connector_name"
     ]
 )
