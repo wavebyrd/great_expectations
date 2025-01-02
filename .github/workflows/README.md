0a8147467ceb352e02483ef82f3f04c9985e6591
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2025-01-02 15:01:10 -0500

    [MAINTENANCE] Nightly cleanup of stray bigquery schemas (#10815)

--- .github/workflows/README.md
+++ .github/workflows/README.md
@@ -6,6 +6,8 @@
   - Responsible for keeping PR's up-to-date with `develop` (only works if "auto-merge" is turned on)
 * [CodeSee Architecture Diagrams](codesee-arch-diagram.yml)
   - Generates a visualization of proposed changes to the codebase through the use of https://www.codesee.io/
+* [DataSource Cleanup](data_source_cleanup.yml)
+  - Responsible for cleaning up stray schemas left behind from tests
 * [StaleBot](stale.yml)
   - Responsible for marking PR's and issues as `stale`
 * [PEP-273 Compatability](test-pep273-compatability.yml)
---
+++ scripts/cleanup/cleanup_big_query.py
@@ -0,0 +1,50 @@
+import logging
+
+from great_expectations.compatibility.pydantic import BaseSettings
+from great_expectations.compatibility.sqlalchemy import TextClause, create_engine
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class BigQueryConnectionConfig(BaseSettings):
+    """Environment variables for BigQuery connection.
+    These are injected in via CI, but when running locally, you may use your own credentials.
+    GOOGLE_APPLICATION_CREDENTIALS must be kept secret
+    """
+
+    GE_TEST_GCP_PROJECT: str
+    GE_TEST_BIGQUERY_DATASET: str
+    GOOGLE_APPLICATION_CREDENTIALS: str
+
+    @property
+    def connection_string(self) -> str:
+        return f"bigquery://{self.GE_TEST_GCP_PROJECT}/{self.GE_TEST_BIGQUERY_DATASET}?credentials_path={self.GOOGLE_APPLICATION_CREDENTIALS}"
+
+
+SCHEMA_FORMAT = "^test_[a-z]{10}$"
+
+
+def cleanup_big_query(config: BigQueryConnectionConfig) -> None:
+    engine = create_engine(url=config.connection_string)
+    with engine.connect() as conn, conn.begin():
+        results = conn.execute(
+            TextClause(
+                """
+                SELECT 'DROP SCHEMA ' || schema_name || ' CASCADE;'
+                FROM INFORMATION_SCHEMA.SCHEMATA
+                WHERE REGEXP_CONTAINS(schema_name, :schema_format)
+                AND creation_time < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 HOUR);
+                """
+            ),
+            {"schema_format": SCHEMA_FORMAT},
+        ).fetchall()
+        to_run = "\n".join([row[0] for row in results])
+        conn.execute(to_run)
+        logger.info(f"Cleaned up {len(results)} BigQuery schema(s)")
+    engine.dispose()
+
+
+if __name__ == "__main__":
+    config = BigQueryConnectionConfig()  # type: ignore[call-arg]  # pydantic populates from env vars
+    cleanup_big_query(config)
