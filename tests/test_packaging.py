2f8a463c2af83a71e043e636376d68602124f66a
Chetan Kini <chetan@superconductive.com>
2025-01-02 11:13:02 -0500

    [MAINTENANCE] Bump `posthog` to V3 (#10814)

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -255,7 +255,7 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev.txt", "marshmallow", (("<", "4.0.0"), (">=", "3.7.1"))),
         ("requirements-dev.txt", "moto", (("<", "5.0"), (">=", "4.2.13"))),
         ("requirements-dev.txt", "pandas", (("<", "2.2.0"),)),
-        ("requirements-dev.txt", "posthog", (("<", "3"), (">=", "2.1.0"))),
+        ("requirements-dev.txt", "posthog", (("<", "4"), (">", "3"))),
         ("requirements-dev.txt", "pyathena", (("<", "3"), (">=", "2.0.0"))),
         ("requirements-dev.txt", "pypd", (("==", "1.1.0"),)),
         ("requirements-dev.txt", "snowflake-sqlalchemy", (("<", "1.7.0"), (">=", "1.2.3"))),
@@ -266,5 +266,5 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements.txt", "altair", (("<", "5.0.0"), (">=", "4.2.1"))),
         ("requirements.txt", "marshmallow", (("<", "4.0.0"), (">=", "3.7.1"))),
         ("requirements.txt", "pandas", (("<", "2.2"),)),
-        ("requirements.txt", "posthog", (("<", "3"), (">=", "2.1.0"))),
+        ("requirements.txt", "posthog", (("<", "4"), (">", "3"))),
     }
