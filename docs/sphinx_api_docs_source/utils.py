c703b9610f2822ca2a7a9b6b5493d6b6faf5a93e
Jessica Saavedra <jessicasaavedraval@gmail.com>
2025-01-27 13:58:02 -0300

    [DOCS] DOC-966: display properties in api reference as tables (#10880)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/sphinx_api_docs_source/utils.py
+++ docs/sphinx_api_docs_source/utils.py
@@ -43,14 +43,28 @@ def apply_structure_changes(soup, html_file_path, html_file_contents):
     if methods:
         add_section_title(soup, methods, "Methods")
 
-    # Add h2 title to Properties section
     properties = soup.select(".py.property")
     if properties:
+        # Add h2 title to Properties section
         add_section_title(soup, properties, "Properties")
+
+        # Display properties as table
+        table = soup.new_tag("table")
+        tbody = soup.new_tag("tbody")
+
+        create_header_row(soup, table)
+
+        for prop in properties:
+            create_row(soup, tbody, prop)
+
+        table.append(tbody)
+        parent_div = soup.select("#properties")[0]
+        parent_div.append(table)
 
 
 def add_section_title(soup, items, title):
     wrapper_div = soup.new_tag("div")
+    wrapper_div.attrs["id"] = title.lower()
     title_h2 = soup.new_tag("h2")
     title_h2.string = title
     parent = items[0].parent
@@ -62,3 +76,43 @@ def add_section_title(soup, items, title):
     wrapper_div.insert(1, title_h2)
     wrapper_div.insert(2, "\r\n")
     parent.insert_after(wrapper_div)
+
+
+def create_header_row(soup, table):
+    thead = soup.new_tag("thead")
+    thead_row = soup.new_tag("tr")
+
+    column_names = ["Name", "Description", "Reference"]
+
+    for column_name in column_names:
+        new_cell = soup.new_tag("th")
+        new_cell.append(column_name)
+        thead_row.append(new_cell)
+
+    thead.append(thead_row)
+
+    table.append(thead)
+    table.append("\r\n")
+    table.insert(0, "\r\n")
+
+
+def create_row(soup, tbody, prop):
+    new_row = soup.new_tag("tr")
+    reference_link = prop.select("a")[0]
+    reference_link.string = reference_link.text.split(".")[-1]
+
+    columns = [
+        "`" + prop.select(".descname")[0].get_text() + "`",
+        prop.select("dd")[0].get_text(),
+        reference_link,
+    ]
+
+    for column in columns:
+        new_cell = soup.new_tag("td")
+        new_cell.append("\r\n")
+        new_cell.append(column)
+        new_cell.append("\r\n")
+        new_row.append(new_cell)
+
+    tbody.append(new_row)
+    prop.extract()
