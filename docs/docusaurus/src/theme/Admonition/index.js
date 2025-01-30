ae1b30dc48d18ce7a6722cb80659dc7bc54166dd
Deborah <38264052+deborahniesz@users.noreply.github.com>
2025-01-30 12:13:19 -0300

    [DOCS] New icons for admonitions (#10899)

--- docs/docusaurus/src/theme/Admonition/index.js
+++ docs/docusaurus/src/theme/Admonition/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Admonition from '@theme-original/Admonition';
-import InfoIcon from "../../../static/img/admonition-info-icon.svg"
+import AlertInfo from "../../../static/img/alert-info.svg";
+import AlertWarning from "../../../static/img/alert-warning.svg";
 import CautionIcon from "../../../static/img/admonition-caution-icon.svg"
+import AlertBell from "../../../static/img/alert-bell.svg";
+import AlertPin from "../../../static/img/alert-pin.svg";
 import DangerIcon from "../../../static/img/admonition-danger-icon.svg"
 import CtaIcon from "../../../static/img/admonition-cta-icon.svg"
 
@@ -9,12 +12,15 @@ export default function AdmonitionWrapper(props) {
 
   switch(props.type) {
     case 'info':
+      return <Admonition {...props} icon={<AlertBell/>} />;
     case 'note':
+      return <Admonition {...props} icon={<AlertPin/>} />;
     case 'tip':
-      return <Admonition {...props} icon={<InfoIcon/>} />;
+      return <Admonition {...props} icon={<AlertInfo/>} />;
     case 'caution':
-    case 'warning':
       return <Admonition {...props} icon={<CautionIcon/>} />;
+    case 'warning':
+      return <Admonition {...props} icon={<AlertWarning/>} />;
     case 'danger':
       return <Admonition {...props} icon={<DangerIcon/>} />;
     case 'cta':
