a29d000e48362267cca6761d7f8a234bc2f44fcb
Aaron Ross <33615526+ashmortar@users.noreply.github.com>
2025-04-07 15:01:28 -0700

    [MAINTENANCE] Improve package autocompletion behavior (#11070)

--- great_expectations/__init__.py
+++ great_expectations/__init__.py
@@ -6,11 +6,21 @@ from ._version import get_versions as _get_versions
 __version__ = _get_versions()["version"]
 del _get_versions
 
-# great_expectations.data_context must be imported first or we will have circular dependency issues
-import great_expectations.data_context  # isort:skip
-import great_expectations.core
+# Submodules must be imported first to avoid circular dependencies
+# with data_context being the first
+from . import data_context  # isort:skip
+from . import core
+from . import exceptions
+from . import expectations
+from . import checkpoint
 
+# Top-level functions/classes promoted to the gx namespace
 from great_expectations.data_context.data_context.context_factory import get_context
+from great_expectations.checkpoint import Checkpoint
+from great_expectations.core.expectation_suite import ExpectationSuite
+from great_expectations.core.result_format import ResultFormat
+from great_expectations.core.run_identifier import RunIdentifier
+from great_expectations.core.validation_definition import ValidationDefinition
 
 # # By placing this registry function in our top-level __init__,  we ensure that all
 # # GX workflows have populated expectation registries before they are used.
@@ -24,13 +34,3 @@ _register_core_expectations()
 
 del _register_core_metrics
 del _register_core_expectations
-
-from great_expectations import exceptions
-from great_expectations import expectations
-from great_expectations import checkpoint
-
-from great_expectations.checkpoint import Checkpoint
-from great_expectations.core.expectation_suite import ExpectationSuite
-from great_expectations.core.result_format import ResultFormat
-from great_expectations.core.run_identifier import RunIdentifier
-from great_expectations.core.validation_definition import ValidationDefinition
