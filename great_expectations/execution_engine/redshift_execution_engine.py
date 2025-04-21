fce68488577f84e9bc736059bdf808872a417c46
David Talbot <17692467+dctalbot@users.noreply.github.com>
2025-04-21 18:51:55 -0400

    [BUGFIX] redshift: prevent runtime TypeError (#11112)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bill Dirks <bill@greatexpectations.io>

--- great_expectations/execution_engine/redshift_execution_engine.py
+++ great_expectations/execution_engine/redshift_execution_engine.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Optional, cast
+from typing import Any, NamedTuple, Optional, cast
 
 from great_expectations.compatibility import aws
 from great_expectations.compatibility.sqlalchemy import sqlalchemy as sa
@@ -16,6 +16,15 @@ from great_expectations.expectations.metrics.metric_provider import metric_value
 from great_expectations.expectations.metrics.table_metrics.table_column_types import (
     ColumnTypes as BaseColumnTypes,
 )
+
+
+class RedshiftColumnSchema(NamedTuple):
+    character_maximum_length: int | None
+    numeric_precision: int | None
+    numeric_scale: int | None
+    datetime_precision: int | None
+    data_type: str
+    column_name: str
 
 
 class RedshiftExecutionEngine(SqlAlchemyExecutionEngine):
@@ -44,8 +53,7 @@ REDSHIFT_TYPES = (
         "character": aws.redshiftdialect.CHAR,
         "character varying": aws.redshiftdialect.VARCHAR,
         "date": aws.redshiftdialect.DATE,
-        # Redshift has this type but the dialect doesn't so we map it to sa.TIME.
-        "time without time zone": sa.TIME,
+        "time without time zone": sa.TIME,  # NOTE: no equivalent in aws.redshiftdialect
         "time with time zone": aws.redshiftdialect.TIMETZ,
         "timestamp without time zone": aws.redshiftdialect.TIMESTAMP,
         "timestamp with time zone": aws.redshiftdialect.TIMESTAMPTZ,
@@ -82,43 +90,43 @@ class ColumnTypes(BaseColumnTypes):
         # For sqlalchemy 2 use this new implementation which avoids incompatible parts of dialect
         # Get the table information
         assert isinstance(execution_engine, RedshiftExecutionEngine)
+        result = []
         table_name, schema_name = cls._get_table_schema(execution_engine, metric_domain_kwargs)
-        full_table_name = f'"{schema_name}"."{table_name}"' if schema_name else f"{table_name}"
-        # Query for the column information
-        query = sa.text(f"""
-            select * from pg_get_cols('{full_table_name}')
-            cols(view_schema name, view_name name, col_name name, col_type varchar, col_num int);
-        """)
-        raw_result = execution_engine.execute_query(query)
-        # Parse out metadata
-        column_metadata = []
-        # The raw result is a tuple of strings, one for each row.
-        for r in raw_result:
-            _schema, _table, column, raw_column_type, _column_num = r
-            # If the type is parameterized, we must removed the arguments to the types to do a
-            # string lookup from the type string to the actual type.
-            column_type_str_parts = raw_column_type.split("(")
-            column_base_type = REDSHIFT_TYPES.get(column_type_str_parts[0])
-            if column_base_type is None:
+        schema_filter: str = f"and table_schema = '{schema_name}'" if schema_name else ""
+        query = sa.text(
+            f"""
+            SELECT
+                column_name,
+                data_type,
+                character_maximum_length,
+                numeric_precision,
+                numeric_scale,
+                datetime_precision
+            FROM information_schema.columns
+            WHERE table_name = '{table_name}' {schema_filter}
+            ORDER BY ordinal_position;
+            """
+        )
+        rows: sa.CursorResult[RedshiftColumnSchema] = execution_engine.execute_query(query)
+
+        for row in rows:
+            redshift_type = REDSHIFT_TYPES.get(row.data_type)
+            if redshift_type is None:
                 raise RedshiftExecutionEngineError(
-                    message=f"Unknown Redshift column type: {raw_column_type}"
+                    message=f"Unknown Redshift column type: {row.data_type}"
                 )
-            # We expect our split on the raw column type to be either:
-            #  length 1: no arguments
-            #  length 2: has arguments
-            # We don't expect any argument nesting.
-            expected_column_type_str_parts = [1, 2]
-            if len(column_type_str_parts) == expected_column_type_str_parts[0]:
-                column_type = column_base_type()
-            elif len(column_type_str_parts) == expected_column_type_str_parts[1]:
-                column_type_args = column_type_str_parts[1].rstrip(")").split(",")
-                column_type = column_base_type(*column_type_args)
-            else:
-                raise RedshiftExecutionEngineError(
-                    message=f"Unexpected nesting of arguments in column type: {raw_column_type}"
-                )
-            column_metadata.append({"name": column, "type": column_type})
-        return column_metadata
+
+            kwargs = cls._get_sqla_column_type_kwargs(row)
+
+            # use EAFP for convenience https://docs.python.org/3/glossary.html#term-EAFP
+            try:
+                column_type = redshift_type(**kwargs)
+            except Exception:
+                column_type = redshift_type()
+
+            result.append({"name": row.column_name, "type": column_type})
+
+        return result
 
     @classmethod
     def _get_table_schema(
@@ -157,3 +165,20 @@ class ColumnTypes(BaseColumnTypes):
             schema_name = batch_data.source_schema_name or batch_data.selectable.schema
 
         return table_selectable, schema_name
+
+    @classmethod
+    def _get_sqla_column_type_kwargs(cls, row: sa.Row[RedshiftColumnSchema]) -> dict:
+        """
+        Build a dict based on mapping redshift column metadata to SQLAlchemy Type named kwargs.
+        """
+        result = {}
+        if row.character_maximum_length is not None:
+            result["length"] = row.character_maximum_length
+        if row.numeric_scale is not None:
+            result["scale"] = row.numeric_scale
+        if row.numeric_precision is not None:
+            result["precision"] = row.numeric_precision
+        if row.datetime_precision is not None and "with time zone" in row.data_type:
+            result["precision"] = row.datetime_precision
+            result["timezone"] = True
+        return result
